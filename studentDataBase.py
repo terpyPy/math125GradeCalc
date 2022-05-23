f387d9f9dc37425575a76edbdad3c3b942a8c5cd
terpyPy <66324329+terpyPy@users.noreply.github.com>
2022-05-23 12:47:41 -0400

    refactor for Sections container.

--- studentDataBase.py
+++ studentDataBase.py
@@ -4,39 +4,40 @@ import random
 import gradeCalc
 
 
-
-def main(numStudents=1000, ui:bool=False)->MathClass:
+def main(numStudents=1000, ui: bool = False) -> MathClass:
     startTime = time.time()
-    schoolClass = MathClass(1, [])
+    schoolClass = MathClass(1)
     # msgs:str = 'Initializing...'
     for i in range(numStudents):
-        # sg.one_line_progress_meter('Making students', 
-        #                            i+1, 
-        #                            numStudents, 
-        #                            'key', 
+        # sg.one_line_progress_meter('Making students',
+        #                            i+1,
+        #                            numStudents,
+        #                            'key',
         #                            msgs,
         #                            makeStudent(schoolClass, i))
         makeStudent(schoolClass, i)
-        
+
     runTime = time.time() - startTime
     print(f'Time to create class: {runTime}')
     schoolClass.sortStudents()
     return schoolClass
-        
-def makeStudent(schoolClass:MathClass, i)->str:
-    test = [[4,3],
-                [
-                    [random.randrange(50,100),
-                    random.randrange(50,100),
-                    random.randrange(50,100)],
-                    random.randrange(50,100)],
-                random.randrange(70,100),
-                random.randrange(60,100)
-                ]
+
+
+def makeStudent(schoolClass: MathClass, i) -> str:
+    test = [[4, 3],
+            [
+        [random.randrange(25, 100),
+         random.randrange(45, 100),
+         random.randrange(65, 100)],
+        random.randrange(30, 100)],
+        random.randrange(40, 100),
+        random.randrange(60, 100)
+    ]
     studentGrades = gradeCalc.uiVersion(test)
     schoolClass.addStudent(i, studentGrades)
-    
+
     # return schoolClass.students[i].getStudentName() + 'loaded...'
-    
+
+
 if __name__ == '__main__':
     x = main(20000)
