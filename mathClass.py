2b2b6a329a90e534e6791c76ba217282a49530eb
terpyPy <66324329+terpyPy@users.noreply.github.com>
2022-05-23 12:45:54 -0400

    refactor for extension by Sections class
    refactored class avg calculations and return values to sections class methods

--- mathClass.py
+++ mathClass.py
@@ -1,37 +1,48 @@
 
+# Author      : Cameron W. Kerley
+# Date        : 5/23/2022
+# Description : is extended by sections class container called "sections"
+#               has access to an instance of Student class variables. will be used to create a grade book
+#               each instance of of a math class will have a list of 
+#               students and return the over all averages and retrieve the student names/IDs
 from Student import Student
-
-# has access to an instance of Student class variables
+import numpy as np
+#
+# TODO: No current TODO's 
 class MathClass(Student):
-    def __init__(self, sectionNumber, debug=None):
-        super().__init__(0, sectionNumber)
-        self.sectionNumber = sectionNumber
+    def __init__(self, sectionNumber, grades='default, default, default, default'):
+        self.sectionNum = sectionNumber
+        super().__init__(0, self.sectionNum, grades)
         self.students:list[Student] = []
         self.names = []
         self.caseDict = {}
     
-    def addStudent(self, ID, currentGrade):
-        self.students.append(Student(ID, self.sectionNumber, currentGrade))
+    def addStudent(self, ID, currentGrade, sectionNumber=None):
+        if sectionNumber is None:
+            self.students.append(Student(ID, self.sectionNum, currentGrade))
+        else:
+            self.students.append(Student(ID, sectionNumber, currentGrade))
     
     def getClassAverage(self)->float:
-        total = 0
-        for student in self.students[1:]:
-            total += student.getClassTotal()
-        self.classTotal = total/len(self.students)
+        self.classTotal = sum([i.getClassTotal() for i in self.students[1:]])/(len(self.students)-1)
         return self.classTotal
+        # for student in self.students[1:]:
+        #     total += student.getClassTotal()
+        # self.classTotal = total/len(self.students)
+        # return self.classTotal
     
     def getTestAverage(self)->float:
-        total = 0
-        for student in self.students[1:]:
-            total += student.getTestTotal()
-        self.testTotal = total/len(self.students)
+        self.testTotal = sum([i.getTestTotal() for i in self.students[1:]])/(len(self.students)-1)
+        # for student in self.students[1:]:
+        #     total += student.getTestTotal()
+        # self.testTotal = total/len(self.students)
         return self.testTotal
 
     def getClassWorkAverage(self)->float:
-        total = 0
-        for student in self.students[1:]:
-            total += student.getClassWorkTotal()
-        self.ClassWorkTotal = total/len(self.students)
+        self.ClassWorkTotal = sum([i.getClassWorkTotal() for i in self.students[1:]])/(len(self.students)-1)
+        # for student in self.students[1:]:
+        #     total += student.getClassWorkTotal()
+        # self.ClassWorkTotal = total/len(self.students)
         return self.ClassWorkTotal
 
     def sortStudents(self):
@@ -47,10 +58,14 @@ class MathClass(Student):
         self.caseDict[case](text)
         self.caseDict.clear()
     
-    def getStudentNames(self)->list[str]:
+    def getStudentNames(self)->'list[str]':
         
         self.names = [i.getStudentName() for i in self.students[1:]]
         return self.names
+    def getStudentPlot(self)->'list[float]':
+        self.plot1 = np.array([i.getClassTotal() for i in self.students[1:]])
+        self.plot2 = np.array([i.getTestTotal() for i in self.students[1:]]) 
+        return self.plot1, self.plot2
         
     def getStudent(self, studentName:int,UI=False):
         # binary search for student
